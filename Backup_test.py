220ed0ea3f77906452127f689190af2b6f915308
jako4 <100273870+jako4@users.noreply.github.com>
2022-03-08 09:37:36 +0100

    it's done

--- Backup_test.py
+++ Backup_test.py
@@ -13,12 +13,12 @@
 # How to open a foldder
 # https://www.youtube.com/watch?v=wmJba8XicU0&t=50s
 
-import datetime
-import shutil
-import os
-import zipfile
+import datetime  # date and time
+import shutil  # copy of files
+import os  # status on files, go to a path
+import zipfile  # zip and compress a file
 
-# Todays date and time
+# Today's date and time
 todays_date = datetime.datetime.now()
 format_date = todays_date.strftime("_%d_%m_%Y_%H%M%S")
 
@@ -29,30 +29,28 @@ new_directory = "Backup" + format_date
 source = 'C:\\files\\data'
 destination = 'C:\\files\\backup\\' + new_directory
 copy_to_dst = shutil.copytree(source, destination)
+print("")
 print("Backup completed. Your files are stored here: " + destination)
 
 # Zip the newly created backup folder
 os.chdir('C:\\files\\backup')
 zip_dir = new_directory
 
-with zipfile.ZipFile(new_directory + ".zip", 'w', zipfile.ZIP_DEFLATED) as newzip:
+with zipfile.ZipFile(zip_dir + ".zip", 'w', zipfile.ZIP_DEFLATED) as newzip:
     for dirpath, dirnames, files in os.walk(zip_dir):
         for file in files:
             newzip.write(os.path.join(dirpath, file))
 
 # Deletes the original backup folder
-os.chdir('C:\\files\\backup')
-delete_folder = "Backup_07_03_2022_214506"
-try:
-    shutil.rmtree(delete_folder)
-except OSError as e:
-    print("Error: %s - %s." % (e.filename, e.strerror))
+# os.chdir('C:\\files\\backup') We are already in this path, otherwise you will need to write this.
+delete_folder = new_directory
+shutil.rmtree(delete_folder)
 
 # User can select Yes or No to open main backup folder
 open_backup_dir: str
 open_backup_dir = input("Do you want to open the main backup folder Y/N: ")
 while open_backup_dir.upper() != "Y" and open_backup_dir.upper() != "N":
-    print(" ")
+    print("")
     print("Type N or Y")
     open_backup_dir = input("Do you want to open the main backup folder Y/N: ")
 
